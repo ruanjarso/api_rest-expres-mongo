46c2513e82645d0ddf93ddc35416cec8a764cfb2
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-04-12 23:40:57 -0300

    create routes

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const routes = [{
+    "/":"rota 1",
+    "/route2":"rota 2",
+    "/route3":"rota3"
+}]
+
 const server = http.createServer((req,res) => {
     res.writeHead(200,{"Content-Type":"text/plain"});
-    res.end("primeiro teste");
+    res.end(routes[req.url]);
 });
 
 server.listen(PORT,() => {
