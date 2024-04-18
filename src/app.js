9145a0e5af025e3fb2d6eea7d6c47e732c94cef1
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-04-18 14:37:57 -0300

    create a GET with id

--- src/app.js
+++ src/app.js
@@ -4,25 +4,39 @@ const app = express();
 app.use(express.json());
 
 const produtos = [{
-    "id": "1",
+    "id": 1,
     "produto":"produto 1",
     "preço": 10,
     "quantidade": 10
 },
 {
-    "id": "2",
+    "id": 2,
     "produto": "produto 2",
     "preço": 20,
     "quantidade": 20
 }]
 
+function buscaProduto (id) {
+    return produtos.findIndex(livro => {
+        return livro.id === Number(id);
+    })
+}
+
 app.get("/produtos",(req,res) => {
     res.status(200).json(produtos);
 });
+
+app.get("/produtos/:id", (req,res) => {
+    const index = buscaProduto(req.params.id);
+    res.status(200).json(produtos[index]);
+})
+
 
 app.post("/produtos",(req,res) => {
     res.status(200).send("produto cadastrado!")
     produtos.push(req.body);
 });
+
+
 
 export default app;
