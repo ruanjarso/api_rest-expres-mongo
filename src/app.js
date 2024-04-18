dac48f584d72f6913e74d4ad745448063b085082
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-04-18 12:10:02 -0300

    create array produtos

--- src/app.js
+++ src/app.js
@@ -3,6 +3,19 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+const produtos = [{
+    "id": "1",
+    "produto":"produto 1",
+    "preço": 10,
+    "quantidade": 10
+},
+{
+    "id": "2",
+    "produto": "produto 2",
+    "preço": 20,
+    "quantidade": 20
+}]
+
 app.get("/",(req,res) => {
     res.status(200).send("TESTE 1");
 });
@@ -11,5 +24,6 @@ app.get("/prox/:id",(req,res) => {
     const index = buscaLivro(req.params.id);
     res.status(200).json(livros[index]);
 });
+
 
 export default app;
